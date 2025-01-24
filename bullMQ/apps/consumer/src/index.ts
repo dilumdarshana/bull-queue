bd946280d08c0fc5eae2eb387023ee32b0768a49
dilumdarshana <dilum.dar@gmail.com>
2025-01-24 15:31:44 +0530

    feat: bullmq consumer added

---
+++ bullMQ/apps/consumer/src/index.ts
@@ -0,0 +1,46 @@
+import { Worker, Job } from 'bullmq';
+import { ENV } from '@bull/config';
+import { ConnectionOptionType } from '@bull/shared-types';
+
+const {
+  REDIS_HOST,
+  REDIS_PORT,
+  REDIS_PASSWORD,
+  QUEUE_NAME,
+} = ENV;
+
+if (!REDIS_HOST ||!REDIS_PORT ||!REDIS_PASSWORD ||!QUEUE_NAME) {
+  throw new Error('Missing required environment variable');
+}
+
+const connection: ConnectionOptionType = {
+  host: REDIS_HOST,
+  port: REDIS_PORT,
+  password: REDIS_PASSWORD
+};
+
+const worker = new Worker(
+  QUEUE_NAME,
+  async (job: Job) => {
+    const { order_id: orderId } = job.data;
+
+    // Simulate job processing logic
+    if (job.name === 'orderJob') {
+      console.log('Order processing started:', orderId);
+      await new Promise((resolve) => setTimeout(resolve, 3000)); // Simulating job processing time
+      console.log('Job proccessed', orderId);
+    } else if (job.name === 'repeatingJob') {
+      console.log('Repeating job executed.');
+    }
+  },
+  { connection },
+);
+
+// Event listeners for job lifecycle
+worker.on('completed', (job) => {
+  console.log(`Job completed: ${job.id}`);
+});
+
+worker.on('failed', (job, err) => {
+  console.error(`Job failed: ${job?.id}, Error: ${err.message}`);
+});
